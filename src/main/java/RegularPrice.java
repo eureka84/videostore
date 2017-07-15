600849ea5980426c03ed7686756d95bb30971f23
Mario Russo <mario.russo@lastminute.com>
2017-07-15 22:55:11 +0200

    regular price cleaned

--- src/main/java/RegularPrice.java
+++ src/main/java/RegularPrice.java
@@ -15,21 +15,9 @@ public class RegularPrice extends Price
   double amount(int daysRented)
   {
     double result =0;
-    switch (code) {
-case Movie.REGULAR:
 result += 2;
 if (daysRented > 2)
 result += (daysRented - 2) * 1.5;
-break;
-case Movie.NEW_RELEASE:
-result += daysRented * 3;
-break;
-case Movie.CHILDRENS:
-result += 1.5;
-if (daysRented > 3)
-result += (daysRented - 3) * 1.5;
-break;
-}
     return result;
   }
 }
