c5f6769ad4144f3b5629cce7bf53bc322befdf53
Mario Russo <mario.russo@lastminute.com>
2017-07-15 22:56:19 +0200

    children cleaned

--- src/main/java/ChildrenPrice.java
+++ src/main/java/ChildrenPrice.java
@@ -16,21 +16,9 @@ public class ChildrenPrice extends Price
   double amount(int daysRented)
   {
     double result =0;
-    switch (code) {
-case Movie.REGULAR:
-result += 2;
-if (daysRented > 2)
-result += (daysRented - 2) * 1.5;
-break;
-case Movie.NEW_RELEASE:
-result += daysRented * 3;
-break;
-case Movie.CHILDRENS:
 result += 1.5;
 if (daysRented > 3)
 result += (daysRented - 3) * 1.5;
-break;
-}
     return result;
   }
 }
