2dbdff97730d4030f6a670bd4c3d6a58393c61a0
Mario Russo <mario.russo@lastminute.com>
2017-07-16 10:41:27 +0200

    high level abstraction

--- src/main/java/Statement.java
+++ src/main/java/Statement.java
@@ -23,32 +23,69 @@ public class Statement
 	public String create() {
 		totalAmount = 0;
 		frequentRenterPoints = 0;
-		Enumeration 		rentals 				= this.rentals.elements ();
-		String 				result 					= "Rental Record for " + getName () + "\n";
+
+		totalAmount = calculateTotalAmount();
+		frequentRenterPoints = frequentFlierPoints();
+
+		String result = rentalsStatements() + statementFooter(this.totalAmount, this.frequentRenterPoints);
 		
+		
+		return result;
+	}
+
+	private String statementFooter(double totalAmount, int frequentRenterPoints)
+	{
+		String result="";
+		result += "You owed " + String.valueOf (totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
+		return result;
+	}
+
+	private double calculateTotalAmount()
+	{
+		double resultAmount = 0;
+		Enumeration rentals 				= this.rentals.elements ();
+
 		while (rentals.hasMoreElements ()) {
 			double 		thisAmount = 0;
 			Rental 		rental = (Rental)rentals.nextElement ();
 
 			thisAmount = rental.amount();
 
+			resultAmount += thisAmount;
+
+		}
+		return resultAmount;
+	}
+
+	private String rentalsStatements()
+	{
+		String 				result 					= "Rental Record for " + getName () + "\n";
+		Enumeration rentals2 				= this.rentals.elements ();
+		while (rentals2.hasMoreElements ()) {
+			Rental 		rental = (Rental)rentals2.nextElement ();
+
+			result += "\t" + rental.getMovie ().getTitle () + "\t"
+				+ String.valueOf (rental.amount()) + "\n";
+
+		}
+		return result;
+	}
+
+	private int frequentFlierPoints()
+	{
+		int frequentRenterPoints = 0;
+		Enumeration rentals 				= this.rentals.elements ();
+		while (rentals.hasMoreElements ()) {
+			Rental 		rental = (Rental)rentals.nextElement ();
 			frequentRenterPoints++;
-			
+
 			if (rental.getMovie ().getPriceCode () == Movie.NEW_RELEASE
 					&& rental.getDaysRented () > 1)
 				frequentRenterPoints++;
-				
-			result += "\t" + rental.getMovie ().getTitle () + "\t"
-								+ String.valueOf (thisAmount) + "\n";
-			totalAmount += thisAmount;
-				
 		}
-		
-		result += "You owed " + String.valueOf (totalAmount) + "\n";
-		result += "You earned " + String.valueOf (frequentRenterPoints) + " frequent renter points\n";
-		
-		
-		return result;
+
+		return frequentRenterPoints;
 	}
 
 	public double totalAmount()
