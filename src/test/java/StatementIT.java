463bfd80755313ffeae2efe2f7ad6cc081294614
Mario Russo <mario.russo@lastminute.com>
2017-07-16 11:50:01 +0200

    added an integration test

--- src/test/java/StatementIT.java
+++ src/test/java/StatementIT.java
@@ -3,12 +3,13 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class VideoStoreTest
+public class StatementIT
 {
 	@Test
 	public void outputFor_MultipleRegularStatement () {
 
-		Statement statement = new Statement("Fred", rentals -> 7.5,rentals -> 3);
+		Statement statement = new Statement("Fred", new RentalsTotalAmountCalculator(),
+																								new RentalsFrequentFlyerCalculator());
 
 		statement.addRental (new Rental (aRegularMovie("Plan 9 from Outer Space"), 1));
 		statement.addRental (new Rental (aRegularMovie("8 1/2"), 2));
---
+++ src/test/java/StatementOutputTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class StatementOutputTest
+{
+	@Test
+	public void outputFor_MultipleRegularStatement () {
+
+		Statement statement = new Statement("Fred", rentals -> 7.5,rentals -> 3);
+
+		statement.addRental (new Rental (aRegularMovie("Plan 9 from Outer Space"), 1));
+		statement.addRental (new Rental (aRegularMovie("8 1/2"), 2));
+		statement.addRental (new Rental (aRegularMovie("Eraserhead"), 3));
+
+		assertThat(statement.create(),is("Rental Record for Fred\n" +
+			"\tPlan 9 from Outer Space\t2.0\n" +
+			"\t8 1/2\t2.0\n" +
+			"\tEraserhead\t3.5\n" +
+			"You owed 7.5\n" +
+			"You earned 3 frequent renter points\n") );
+	}
+
+	private Movie aRegularMovie(String title)
+	{
+		return new Movie (title, Movie.REGULAR);
+	}
+
+}
